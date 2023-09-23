2a40af826973c9a8d2711b72fbba85ed7b7d43eb
dneumann42 <dustinneumann42@hotmail.com>
2023-09-23 14:20:34 -0700

    more function generation

--- src/coral/core/plugins.nim
+++ src/coral/core/plugins.nim
@@ -1,38 +1,28 @@
-import std/logging, strutils, sequtils, tables
+import std/logging, strutils, sequtils, tables, typetraits
 import sugar, events, tables, patty, options, commands, states, ents
 import ../artist/artist
 
 import std/macros
 import std/macrocache
 
-import fusion/matching
-{.experimental: "caseStmtMacros".}
+proc perm[T](a: openarray[T], n: int, use: var seq[bool]): seq[seq[T]] =
+  result = newSeq[seq[T]]()
+  if n <= 0: return
+  for i in 0 .. a.high:
+    if not use[i]:
+      if n == 1:
+        result.add(@[a[i]])
+      else:
+        use[i] = true
+        for j in perm(a, n - 1, use):
+          result.add(a[i] & j)
+        use[i] = false
 
-type Fn = () -> void
-type FnE = (var Events) -> void
-
-type FnC = (var Commands) -> void
-type FnS = (var GameState) -> void
-type FnCS = (var Commands, var GameState) -> void
-
-type FnA = (var Artist) -> void
-type FnAS = (var Artist, var GameState) -> void
-
-type FnAN = (var Artist, var Ents) -> void
-type FnASN = (var Artist, var GameState, var Ents) -> void
-
-type FnN = (var Ents) -> void
-type FnCN = (var Commands, var Ents) -> void
-type FnSN = (var GameState, var Ents) -> void
-type FnCSN = (var Commands, var GameState, var Ents) -> void
-
-type SomeFunc = Fn | FnE | FnC | FnS | FnCS | FnA | FnAS | FnAN | FnASN | FnN | FnCN | FnSN | FnCSN
+proc permutations[T](a: openarray[T], n: int): seq[seq[T]] =
+  var use = newSeq[bool](a.len)
+  perm(a, n, use)
 
 const typeTable = CacheTable"typeTable"
-
-const FUNCTION_LITERALS = [
-  "Fn", "FnE", "FnC", "FnS", "FnCS", "FnA", "FnAS", "FnAN", "FnASN", "FnN", "FnCN", "FnSN", "FnCSN"
-]
 
 static:
   typeTable["a"] = ident("Artist")
@@ -41,10 +31,65 @@ static:
   typeTable["c"] = ident("Commands")
   typeTable["n"] = ident("Ents")
 
+const FUNCTION_NAMES = block:
+  let keys = collect(for k, _ in typeTable: k)
+  permutations(keys, 1).mapIt(it.join())
+    .concat(permutations(keys, 2).mapIt(it.join()))
+    .concat(permutations(keys, 3).mapIt(it.join()))
+    .concat(permutations(keys, 4).mapIt(it.join()))
+    .concat(permutations(keys, 5).mapIt(it.join()))
+    .mapIt("Fn" & it.toUpperAscii)
+    .concat(@["Fn"])
+
+macro generateFunctionTypes() =
+  var sec = nnkTypeSection.newTree()
+
+  for fn in FUNCTION_NAMES:
+    var def = nnkTypeDef.newTree(ident(fn), newEmptyNode())
+    var prc = nnkProcTy.newTree()
+    var params = nnkFormalParams.newTree(ident("void"))
+
+    for i in 2..<fn.len:
+      var ch = fn[i].toLowerAscii
+      var id = nnkIdentDefs.newTree(ident($ch))
+      id.add(nnkVarTy.newTree(typeTable[$ch]))
+      id.add(newEmptyNode())
+      params.add(id)
+
+    prc.add(params)
+    prc.add(newEmptyNode())
+
+    def.add(prc)
+    sec.add(def)
+
+  nnkStmtList.newTree(sec)
+
+macro generateSomeFunc() =
+  var res = nnkStmtList.newTree()
+  var sec = nnkTypeSection.newTree()
+  
+  proc idx(vs: seq[string]): NimNode =
+    if len(vs) == 1:
+      ident(vs[0])
+    else:
+      nnkInfix.newTree(ident("|"), ident(vs[0]), idx(vs[1..<vs.len]))
+  
+  var def = nnkTypeDef.newTree(
+    newIdentNode("SomeFunc"),
+    newEmptyNode(),
+    idx(FUNCTION_NAMES))
+
+  sec.add(def)
+  res.add(sec)
+  res
+
+generateFunctionTypes()
+generateSomeFunc()
+
 macro generateEnum() =
   newEnum(
     name = ident("FunKind"),
-    fields = FUNCTION_LITERALS.map(
+    fields = FUNCTION_NAMES.map(
       (f) => f[0] & (f[2..<f.len])
     ).mapIt(ident(it.toLowerAscii)),
     public = false, pure = false)
@@ -52,7 +97,7 @@ macro generateEnum() =
 macro generateObject() =
   var xs = nnkRecCase.newTree(
     nnkIdentDefs.newTree(ident("kind"), ident("FunKind"), newEmptyNode()))
-  for f in FUNCTION_LITERALS:
+  for f in FUNCTION_NAMES:
     let field = ident((f[0] & (f[2..<f.len])).toLowerAscii)
     xs.add(nnkOfBranch.newTree(
       field,
@@ -69,7 +114,7 @@ macro generateObject() =
 
 macro generateConstructor() =
   var tree = nnkWhenStmt.newTree()
-  for f in FUNCTION_LITERALS:
+  for f in FUNCTION_NAMES:
     let field = ident((f[0] & (f[2..<f.len])).toLowerAscii)
     tree.add(
       nnkElifBranch.newTree(
@@ -114,7 +159,7 @@ macro generateCall*() =
       newIdentNode("fn"),
       newIdentNode("kind")))
 
-  for f in FUNCTION_LITERALS:
+  for f in FUNCTION_NAMES:
     let id = (f[0] & (f[2..<f.len])).toLowerAscii
     let num = len(id) - 1
 
@@ -141,6 +186,7 @@ macro generateCall*() =
       newEmptyNode(),
       nnkStmtList.newTree(cas)))
 
+
 generateEnum()
 generateObject()
 generateConstructor()
@@ -162,7 +208,7 @@ type
   Plugins* = ref object
     plugins: Table[string, Plugin]
 
-proc impl[T](ps: Plugins, id: string, stage: PluginStage, f: T): Plugins =
+proc impl(ps: Plugins, id: string, stage: PluginStage, f: SomeFunc): Plugins =
   block:
     discard ps.plugins.hasKeyOrPut(id, Plugin(fs: @[]))
     ps.plugins[id].fs.add((stage, f.asFun()))
@@ -174,22 +220,22 @@ type PluginAdd = ref object
 
 macro generateAdds() =
   result = nnkStmtList.newTree()
-  for f in FUNCTION_LITERALS:
-      result.add nnkProcDef.newTree(
-        nnkPostfix.newTree(ident("*"), ident("add")),
-        newEmptyNode(),
-        newEmptyNode(),
-        nnkFormalParams.newTree(
-          ident("Plugins"),
-          nnkIdentDefs.newTree(ident("ps"), ident("Plugins"), newEmptyNode()),
-          nnkIdentDefs.newTree(ident("id"), ident("string"), newEmptyNode()),
-          nnkIdentDefs.newTree(ident("stage"), ident("PluginStage"), newEmptyNode()),
-          nnkIdentDefs.newTree(ident("f"), ident(f), newEmptyNode())),
-        nnkPragma.newTree(ident("discardable")),
-        newEmptyNode(),
-        nnkStmtList.newTree(
-          nnkCall.newTree(ident("impl"), ident("ps"), ident("id"), ident("stage"), ident("f"))))
-    
+  for f in FUNCTION_NAMES:
+    result.add nnkProcDef.newTree(
+      nnkPostfix.newTree(ident("*"), ident("add")),
+      newEmptyNode(),
+      newEmptyNode(),
+      nnkFormalParams.newTree(
+        ident("Plugins"),
+        nnkIdentDefs.newTree(ident("ps"), ident("Plugins"), newEmptyNode()),
+        nnkIdentDefs.newTree(ident("id"), ident("string"), newEmptyNode()),
+        nnkIdentDefs.newTree(ident("stage"), ident("PluginStage"), newEmptyNode()),
+        nnkIdentDefs.newTree(ident("f"), ident(f), newEmptyNode())),
+      nnkPragma.newTree(ident("discardable")),
+      newEmptyNode(),
+      nnkStmtList.newTree(
+        nnkCall.newTree(ident("impl"), ident("ps"), ident("id"), ident("stage"), ident("f"))))
+  
 generateAdds()
 
 iterator items*(ps: Plugins): (string, Plugin) =
@@ -270,3 +316,10 @@ template scene*(ps: Plugins, id: string, lod: untyped): Plugins =
   discard ps.plugins.hasKeyOrPut(id, Plugin(fs: @[], isScene: true,
       hasLoaded: false))
   ps.add(id, load, lod)
+
+when isMainModule:
+  proc drawFn(a: var Artist, c: var Commands) =
+    discard
+
+  var plugins = Plugins.init()
+  plugins.add("test", draw, drawFn)
