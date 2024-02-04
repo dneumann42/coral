a025cb229706827fdbb8c317a730c947efa650ca
dneumann <dneumann@mhighpoint.com>
2024-02-04 09:15:56 -0800

    renamed draw fns and move more over from platform

--- src/coral/artist/artist.nim
+++ src/coral/artist/artist.nim
@@ -77,22 +77,81 @@ proc init*(T: type Artist): T =
   T(camera: Camera.init(),
     layers: @[])
 
+template withClip*(x, y, w, h: SomeNumber, blk: untyped) =
+  application.withClip(x, y, w, h, blk)
+
+proc intersects*(a, b: Vec2, radius = 10.0): bool =
+  dist(a, b) < radius
+
+proc intersects*(a, b: tuple[x, y, w, h: SomeNumber]): bool =
+  a.x + a.w > b.x and a.x < b.x + b.w and a.y + a.h > b.y and a.y < b.y + b.h
+
+proc intersects*(p: Vec2, a: tuple[x, y, w, h: float|float32]): bool =
+  p.x > a.x and p.x < a.x + a.w and p.y > a.y and p.y < a.y + a.h
+
+proc intersects*(p: Vec2, x, y, w, h: float|float32): bool =
+  p.x > x and p.x < x + w and p.y > y and p.y < y + h
+
 proc size*(layer: Layer): Vec2 =
   let (x, y) = layer.target.size()
   result.x = x.float
   result.y = y.float
 
-proc circle*(x, y: SomeNumber; r = 32.0; color = color(1.0, 1.0, 1.0, 1.0)) =
+proc drawLine*(startX, startY, endX, endY: SomeNumber, color = color(1.0, 1.0, 1.0, 1.0)) =
+  application.line(startX, startY, endX, endY, color)
+
+proc drawCircle*(x, y: SomeNumber; r = 32.0; color = color(1.0, 1.0, 1.0, 1.0)) =
   application.circle(x, y, r, color)
 
-proc rect*(x, y: SomeNumber; w = 64.0; h = 64.0; origin = vec2();
+proc drawCircle*(pos: Vec2; r = 32.0; color = color(1.0, 1.0, 1.0, 1.0)) =
+  application.circle(pos.x, pos.y, r, color)
+
+proc drawRect*(x, y: SomeNumber; w = 64.0; h = 64.0; origin = vec2();
     rotation = 0.0; color = color(1.0, 1.0, 1.0, 1.0)) =
   application.rect(x.float32, y.float32, w.float32, h.float32, origin, rotation, color)
 
-proc linerect*(x, y: SomeNumber; w = 64.0; h = 64.0; origin = vec2();
+proc drawRect*(pos, size: Vec2; origin = vec2();
     rotation = 0.0; color = color(1.0, 1.0, 1.0, 1.0)) =
-  application.linerect(x.float32, y.float32, w.float32, h.float32, origin,
-      rotation, color)
+  application.rect(pos.x, pos.y, size.x, size.y, origin, rotation, color)
+
+proc drawLinerect*(x, y: SomeNumber; w = 64.0; h = 64.0; origin = vec2();
+    rotation = 0.0; color = color(1.0, 1.0, 1.0, 1.0)) =
+  application.linerect(x.float32, y.float32, w.float32, h.float32, origin, rotation, color)
+
+proc drawLinerect*(pos, size: Vec2; origin = vec2();
+    rotation = 0.0; color = color(1.0, 1.0, 1.0, 1.0)) =
+  application.linerect(pos.x, pos.y, size.x, size.y, origin, rotation, color)
+
+proc drawTexture*(
+  textureId: string,
+  src: Rectangle,
+  dst: Rectangle,
+  origin = vec2(),
+  rotation = 0.0,
+  color = color(1.0, 1.0, 1.0, 1.0)
+) =
+  application.texture(textureId, src, dst, origin, rotation, color)
+
+proc drawTexture*(
+  textureId: string,
+  pos: Vec2,
+  scale = 1.0,
+  origin = vec2(),
+  rotation = 0.0,
+  color = color(1.0, 1.0, 1.0, 1.0)
+) =
+  application.texture(textureId, pos, scale, origin, rotation, color)
+
+proc measureString*(text: string, fontId: string): Vec2 =
+  application.measureString(text, fontId)
+
+proc drawText*(
+  tex: string,
+  fontId: string, # TODO: font id should be distinct from string
+  x, y: SomeNumber,
+  color = color(1.0, 1.0, 1.0, 1.0)
+) =
+  application.text(tex, fontId, x, y, color)
 
 proc getOrCreateLayer*(artist: var Artist; depth: int; camera = false): Layer =
   for layer in artist.layers:
